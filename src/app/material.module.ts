9c59717a91b6b4cead56f8853511f1e2ddc743bb
Ildefonso Valenzuela <ponchovalo@outlook.com>
2023-05-19 06:41:36 -0600

    last changes

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,16 +3,19 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon'
 import { MatInputModule } from '@angular/material/input'
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   imports: [MatButtonModule,
             MatIconModule,
             MatInputModule,
-            MatFormFieldModule],
+            MatFormFieldModule,
+            MatSidenavModule],
   exports: [MatButtonModule,
             MatIconModule,
             MatInputModule,
-            MatFormFieldModule]
+            MatFormFieldModule,
+            MatSidenavModule]
 })
 
 
